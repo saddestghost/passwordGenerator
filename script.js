fc44d221b69705662aea1374bc4f621659305e13
saddestghost <will.torrence@gmail.com>
2021-12-09 19:29:46 -0500

    attempting to establish pass length

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,8 +23,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+  var passwordLength = (8 - 128)////////////
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * characters.length);
+        password += characters.substring(randomNumber, randomNumber +1);
+    }
+    
+    document.getElementById("password").value = password;
+    
+    }
+  
 
   passwordText.value = password;
+
+
 
 }
 
